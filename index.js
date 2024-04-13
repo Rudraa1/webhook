316263db165a15f4e9c81a9370f5ffd73898ccbe
Mohit <mohit.saini713.ms@gmail.com>
2024-04-13 13:23:28 +0530

    CUSTOM MSG

--- index.js
+++ index.js
@@ -30,8 +30,6 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-  //i want some
-
   let body_param = req.body;
 
   console.log(JSON.stringify(body_param, null, 2));
@@ -44,40 +42,104 @@ app.post("/webhook", (req, res) => {
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
     ) {
-      let phon_no_id =
+      let phone_number_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      let message_body =
+        body_param.entry[0].changes[0].value.messages[0].text.body;
 
-      console.log("phone number " + phon_no_id);
-      console.log("from " + from);
-      console.log("boady param " + msg_body);
+      console.log("phone number: " + phone_number_id);
+      console.log("from: " + from);
+      console.log("body param: " + message_body);
+
+      // Define specific replies based on message content
+      let reply_message;
+      if (message_body.includes("I want to place order")) {
+        reply_message =
+          "Okay, please provide the name and quantity of the product you want to order.";
+      } else {
+        reply_message = `Hi.. I'm Prasath, your message is ${message_body}`;
+      }
 
       axios({
         method: "POST",
-        url:
-          "https://graph.facebook.com/v13.0/" +
-          phon_no_id +
-          "/messages?access_token=" +
-          token,
+        url: `https://graph.facebook.com/v13.0/${phone_number_id}/messages?access_token=${token}`,
         data: {
           messaging_product: "whatsapp",
           to: from,
           text: {
-            body: "Hi.. I'm Prasath, your message is " + msg_body,
+            body: reply_message,
           },
         },
         headers: {
           "Content-Type": "application/json",
         },
-      });
-
-      res.sendStatus(200);
+      })
+        .then(() => {
+          console.log("Reply sent successfully");
+          res.sendStatus(200);
+        })
+        .catch((error) => {
+          console.error("Error sending reply:", error);
+          res.status(500).send("Error sending reply");
+        });
     } else {
       res.sendStatus(404);
     }
+  } else {
+    res.sendStatus(400);
   }
 });
+
+// app.post("/webhook", (req, res) => {
+//   //i want some
+
+//   let body_param = req.body;
+
+//   console.log(JSON.stringify(body_param, null, 2));
+
+//   if (body_param.object) {
+//     console.log("inside body param");
+//     if (
+//       body_param.entry &&
+//       body_param.entry[0].changes &&
+//       body_param.entry[0].changes[0].value.messages &&
+//       body_param.entry[0].changes[0].value.messages[0]
+//     ) {
+//       let phon_no_id =
+//         body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//       let from = body_param.entry[0].changes[0].value.messages[0].from;
+//       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+//       console.log("phone number " + phon_no_id);
+//       console.log("from " + from);
+//       console.log("boady param " + msg_body);
+
+//       axios({
+//         method: "POST",
+//         url:
+//           "https://graph.facebook.com/v13.0/" +
+//           phon_no_id +
+//           "/messages?access_token=" +
+//           token,
+//         data: {
+//           messaging_product: "whatsapp",
+//           to: from,
+//           text: {
+//             body: "Hi.. I'm Prasath, your message is " + msg_body,
+//           },
+//         },
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       res.sendStatus(200);
+//     } else {
+//       res.sendStatus(404);
+//     }
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.status(200).send("hello this is webhook setup");
